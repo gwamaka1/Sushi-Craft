7aa2a5046ac2c4226dc278060da4f218035cf279
gwamaka1 <mwamtobegwamaka@gmail.com>
2026-05-22 14:10:28 -0400

    Created Skeleton code for drink class and intialized instance variablkes

--- src/main/java/com/pluarsight/models/Drink.java
+++ src/main/java/com/pluarsight/models/Drink.java
@@ -1,4 +1,30 @@
 package com.pluarsight.models;
 
-public class Drink {
+public class Drink implements IOrderItem {
+    private String size;
+    private String flavor;
+
+    public Drink(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+
+    @Override
+    public String getDetails() {
+        return "";
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
 }
