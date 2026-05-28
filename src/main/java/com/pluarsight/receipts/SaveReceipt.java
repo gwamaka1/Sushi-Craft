d2f09e4723f11820199a0bd0d111835b02ce9604
gwamaka1 <mwamtobegwamaka@gmail.com>
2026-05-28 17:16:11 -0400

    forgot to create a directory to the folder if it dont exists. but i just added it

--- src/main/java/com/pluarsight/receipts/SaveReceipt.java
+++ src/main/java/com/pluarsight/receipts/SaveReceipt.java
@@ -13,6 +13,10 @@ public class SaveReceipt {
     public void save(CustomerPurchase customerPurchase) {
         String folderPath = "C:\\Users\\mwamt\\pluarsight\\java-dev\\capstone\\Sushi-Craft\\reciepts";
         File folder = new File(folderPath);
+        // Create the directory if it doesn't exist
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter ft = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
         String out= now.format(ft) + ".txt";
@@ -25,9 +29,13 @@ public class SaveReceipt {
             bw.newLine();
             bw.write(customerPurchase.getDetails());
             bw.newLine();
+            bw.write(String.format("Total: $%.2f", customerPurchase.getTotal()));
+            bw.newLine();
             bw.write("-------------------");
             bw.newLine();
+            bw.close();
         } catch (IOException e) {
+            System.err.println("Failed to save receipt: " + e.getMessage());
             throw new RuntimeException(e);
         }
 
